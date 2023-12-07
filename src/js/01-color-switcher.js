1a8a33f8b26c308fe55fb82726b346775569a7ba
olga-deputat <144669008+olga-deputat@users.noreply.github.com>
2023-12-07 12:38:58 +0200

    1
    додала стила на кнопки

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,13 +2,13 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
 let colorInterval;
 
 //роблю функцію: задаю кнопки, активність кнопок, інтервал 1сек
-function startColorSwitch() {
-  const startButton = document.querySelector('[data-start]');
-  const stopButton = document.querySelector('[data-stop]');
-
+function startColor() {
   startButton.disabled = true;
   stopButton.disabled = false;
 
@@ -16,14 +16,12 @@ function startColorSwitch() {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 }
-
+startButton.addEventListener('click', startColor);
 //функція для зупинки кольору:кнопки, актиіність кнопок навпаки, зупиняю інтервал
-function stopColorSwitch() {
-  const startButton = document.querySelector('[data-start]');
-  const stopButton = document.querySelector('[data-stop]');
-
+function stopColor() {
   startButton.disabled = false;
   stopButton.disabled = true;
 
   clearInterval(colorInterval);
 }
+stopButton.addEventListener('click', stopColor);
