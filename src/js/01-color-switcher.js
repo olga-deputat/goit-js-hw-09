762796e6839e7a1a31b2190e47f95e1eaa36bad9
olga-deputat <144669008+olga-deputat@users.noreply.github.com>
2023-12-07 11:51:59 +0200

    завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
 
+let colorInterval;
+
+//роблю функцію: задаю кнопки, активність кнопок, інтервал 1сек
+function startColorSwitch() {
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  colorInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+//функція для зупинки кольору:кнопки, актиіність кнопок навпаки, зупиняю інтервал
+function stopColorSwitch() {
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  clearInterval(colorInterval);
+}
